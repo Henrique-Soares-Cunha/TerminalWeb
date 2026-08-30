e944d2f10467fde820fdd4d00d061db722a69c77
Henrique <henrique.sc2@puccampinas.edu.br>
2026-08-30 18:11:49 -0300

    Terminal web v1.1: inicial settings for new features

--- webAutomationScript.py
+++ webAutomationScript.py
@@ -4,48 +4,74 @@ import os
 import sys
 import urllib.parse
 
+def serchManager( alvo , is_site_listed : bool):
+    if (is_site_listed == True):
+        # Get the website URL from the sites variable
+        url = sites[alvo]
+    else:
+        termo = urllib.parse.quote_plus(alvo)
+        url = f"https://www.google.com/search?q={termo}"
+
+    # Open a connection with the browser and open the site via the URL
+    browser_connection = webbrowser.open(url, 2, True)
+
+    # Check browser connection status
+    if not browser_connection:
+        print("Erro de conexão com o navegador.")
+        sys.exit(1)
+    else:
+        print(f"Abridor web: '{alvo}' aberto com sucesso!")
+
+def geminiQuestion( question ):
+    print("função em desencolvimento")
+    print(sys.argv[2].lower())
+    #vai conectar com o gemini, fazer uma pergunta, printar a resposta parar de executar
+    # a fazer
+    sys.exit(1)
+
 #Script to open the browser and automatically search for the site using the terminal
 if __name__ == '__main__':
 
     #Ensure the command has the minimum required arguments
-    if (len(sys.argv) < 2):
+    tamanho = len(sys.argv)
+    if (tamanho < 2 or tamanho > 3):
         print ("Uso incorreto! Digite: web <nome_do_site>")
         sys.exit(1)
 
-    #Get the second argument of the command
-    alvo = sys.argv[1].lower()
+    if (tamanho == 3):
+        try:
+            comando = sys.argv[1].lower()
+            match comando:
+                case "gemini":
+                    geminiQuestion( sys.argv[2].lower())
+                case _:
+                    print(f"comando ${comando} invalido")
+                    sys.exit(1)
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+            sys.exit(1)
 
-    try:
-        #Get the current directory path
-        diretorio_script = os.path.dirname(os.path.abspath(__file__))
-        #Use the obtained path and append the sites.json file
-        caminho_json = os.path.join(diretorio_script, "sites.json")
+    else:
+        #Get the second argument of the command
+        alvo = sys.argv[1].lower()
 
-        #Open sites.json and save it to a variable
-        with open(caminho_json, "r", encoding="utf-8") as arquivo:
-            sites = json.load(arquivo)
+        try:
+            #Get the current directory path
+            diretorio_script = os.path.dirname(os.path.abspath(__file__))
+            #Use the obtained path and append the sites.json file
+            caminho_json = os.path.join(diretorio_script, "sites.json")
 
-        #Check if the alvo exists in sites
-        if alvo in sites:
-            #Get the website URL from the sites variable
-            url = sites[alvo]
-            #Open a connection with the browser and open the site via the URL
-            browser_connection = webbrowser.open(url, 2, True)
+            #Open sites.json and save it to a variable
+            with open(caminho_json, "r", encoding="utf-8") as arquivo:
+                sites = json.load(arquivo)
 
-            if not browser_connection:
-                print("Erro de conexão com o navegador.")
-            else:
-                print(f"Abridor web: '{alvo}' aberto com sucesso!")
-        else:
-            print(f"O site '{alvo}' não está cadastrado no sites.json.")
-            print(f"Deseja procurar o site diretamente no google?S/n")
-            resposta = input()
-            if resposta.lower() != 'n':
-                termo = urllib.parse.quote_plus(alvo)
-                url = f"https://www.google.com/search?q={termo}"
-                webbrowser.open(url, 2 , True)
+            #Check if the alvo exists in sites
+            is_site_listed = alvo in sites
+            serchManager(alvo, is_site_listed)
 
-    except FileNotFoundError:
-        print("Erro: Arquivo 'sites.json' não encontrado.")
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+        except FileNotFoundError:
+            print("Erro: Arquivo 'sites.json' não encontrado.")
+            sys.exit(1)
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+            sys.exit(1)
